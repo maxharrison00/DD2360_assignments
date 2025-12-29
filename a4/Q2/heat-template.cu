8c7fc2efa5e83a9cf26f5ab195e6d5415e97df75
Max Harrison <maxlucashar@gmail.com>
2025-12-29 14:00:22 +0100

    Finished Q2

--- a4/Q2/heat-template.cu
+++ a4/Q2/heat-template.cu
@@ -1,8 +1,10 @@
 #include <cublas_v2.h>
 #include <cuda_runtime_api.h>
-#include <cusparse_v2.h>
+#include <cusparse.h>
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 #include <thrust/device_ptr.h>
 #include <thrust/sequence.h>
@@ -16,7 +18,6 @@
     }                                                                          \
   } while (0)
 
-// Macro to check the cuBLAS status
 #define cublasCheck(stmt)                                                      \
   do {                                                                         \
     cublasStatus_t err = stmt;                                                 \
@@ -26,7 +27,6 @@
     }                                                                          \
   } while (0)
 
-// Macro to check the cuSPARSE status
 #define cusparseCheck(stmt)                                                    \
   do {                                                                         \
     cusparseStatus_t err = stmt;                                               \
@@ -42,218 +42,170 @@ void cputimer_stop(const char *info) {
   gettimeofday(&t_end, 0);
   double time = (1000000.0 * (t_end.tv_sec - t_start.tv_sec) + t_end.tv_usec -
                  t_start.tv_usec);
-  printf("Timing - %s. \t\tElasped %.0f microseconds \n", info, time);
+  printf("Timing - %s. \t\tElapsed %.0f microseconds \n", info, time);
 }
 
-// Initialize the sparse matrix needed for the heat time step
+// Initialize the sparse matrix
 void matrixInit(double *A, int *ArowPtr, int *AcolIndx, int dimX,
                 double alpha) {
-  // Stencil from the finete difference discretization of the equation
   double stencil[] = {1, -2, 1};
-  // Variable holding the position to insert a new element
   size_t ptr = 0;
-  // Insert a row of zeros at the beginning of the matrix
-  ArowPtr[0] = ptr;
-  // Fill the non zero entries of the matrix
+  ArowPtr[1] = ptr;
   for (int i = 1; i < (dimX - 1); ++i) {
-    // Insert the elements: A[i][i-1], A[i][i], A[i][i+1]
     for (int k = 0; k < 3; ++k) {
-      // Set the value for A[i][i+k-1]
       A[ptr] = stencil[k];
-      // Set the column index for A[i][i+k-1]
       AcolIndx[ptr++] = i + k - 1;
     }
-    // Set the number of newly added elements
-    ArowPtr[i] = ptr;
+    ArowPtr[i + 1] = ptr;
   }
-  // Insert a row of zeros at the end of the matrix
-  ArowPtr[dimX - 1] = ptr;
   ArowPtr[dimX] = ptr;
 }
 
 int main(int argc, char **argv) {
-  int device = 0;              // Device to be used
-  int dimX;                    // Dimension of the metal rod
-  int nsteps;                  // Number of time steps to perform
-  double alpha = 0.4;          // Diffusion coefficient
-  double *temp;                // Array to store the final time step
-  double *A;                   // Sparse matrix A values in the CSR format
-  int *ARowPtr;                // Sparse matrix A row pointers in the CSR format
-  int *AColIndx;               // Sparse matrix A col values in the CSR format
-  int nzv;                     // Number of non zero values in the sparse matrix
-  double *tmp;                 // Temporal array of dimX for computations
-  size_t bufferSize = 0;       // Buffer size needed by some routines
-  void *buffer = nullptr;      // Buffer used by some routines in the libraries
-  int concurrentAccessQ;       // Check if concurrent access flag is set
-  double zero = 0;             // Zero constant
-  double one = 1;              // One constant
-  double norm;                 // Variable for norm values
-  double error;                // Variable for storing the relative error
-  double tempLeft = 200.;      // Left heat source applied to the rod
-  double tempRight = 300.;     // Right heat source applied to the rod
-  cublasHandle_t cublasHandle; // cuBLAS handle
-  cusparseHandle_t cusparseHandle; // cuSPARSE handle
-  cusparseSpMatDescr_t matA;       // Sparse matrix descriptor for generic API
-  cusparseDnVecDescr_t vecTemp;    // Dense vector descriptor for temp
-  cusparseDnVecDescr_t vecTmp;     // Dense vector descriptor for tmp
+  int device = 0;
+  int dimX;
+  int nsteps;
+  double alpha = 0.4;
+  double *temp;
+  double *A;
+  int *ARowPtr;
+  int *AColIndx;
+  int nzv;
+  double *tmp;
+  size_t bufferSize = 0;
+  void *buffer = nullptr;
+  int concurrentAccessQ;
+  double zero = 0;
+  double one = 1;
+  double norm;
+  double error;
+  double tempLeft = 200.;
+  double tempRight = 300.;
 
-  // Read the arguments from the command line
+  cublasHandle_t cublasHandle;
+  cusparseHandle_t cusparseHandle;
+  cusparseSpMatDescr_t matA;
+  cusparseDnVecDescr_t vecX, vecY;
+
+  // SpMV timing
+  cudaEvent_t spmv_start, spmv_stop;
+  float spmv_time_ms = 0.0f;
+  float spmv_total_time_ms = 0.0f;
+
   dimX = atoi(argv[1]);
   nsteps = atoi(argv[2]);
 
-  // Print input arguments
   printf("The X dimension of the grid is %d \n", dimX);
   printf("The number of time steps to perform is %d \n", nsteps);
 
-  // Get if the cudaDevAttrConcurrentManagedAccess flag is set
   gpuCheck(cudaDeviceGetAttribute(&concurrentAccessQ,
                                   cudaDevAttrConcurrentManagedAccess, device));
 
-  // Calculate the number of non zero values in the sparse matrix. This number
-  // is known from the structure of the sparse matrix
   nzv = 3 * dimX - 6;
 
-  //@@ Insert the code to allocate the temp, tmp and the sparse matrix
-  //@@ arrays using Unified Memory
-  cputimer_start();
+  // Unified Memory allocation
+  gpuCheck(cudaMallocManaged(&temp, sizeof(double) * dimX));
+  gpuCheck(cudaMallocManaged(&tmp, sizeof(double) * dimX));
+  gpuCheck(cudaMallocManaged(&A, sizeof(double) * nzv));
+  gpuCheck(cudaMallocManaged(&ARowPtr, sizeof(int) * (dimX + 1)));
+  gpuCheck(cudaMallocManaged(&AColIndx, sizeof(int) * nzv));
 
-  cudaMallocManaged(&temp, dimX * sizeof(double));
-  cudaMallocManaged(&tmp, dimX * sizeof(double));
-  cudaMallocManaged(&A, nzv * sizeof(double));
-  cudaMallocManaged(&ARowPtr, (dimX + 1) * sizeof(int));
-  cudaMallocManaged(&AColIndx, nzv * sizeof(int));
-
-  cputimer_stop("Allocating device memory");
-
-  // Check if concurrentAccessQ is non zero in order to prefetch memory
   if (concurrentAccessQ) {
-    cputimer_start();
-    //@@ Insert code to prefetch in Unified Memory asynchronously to CPU
-    cudaMemPrefetchAsync(A, nzv * sizeof(double), cudaCpuDeviceId, 0);
-    cudaMemPrefetchAsync(ARowPtr, (dimX + 1) * sizeof(int), cudaCpuDeviceId, 0);
-    cudaMemPrefetchAsync(AColIndx, nzv * sizeof(int), cudaCpuDeviceId, 0);
-    cputimer_stop("Prefetching GPU memory to the host");
+    gpuCheck(
+        cudaMemPrefetchAsync(temp, sizeof(double) * dimX, cudaCpuDeviceId));
+    gpuCheck(cudaMemPrefetchAsync(tmp, sizeof(double) * dimX, cudaCpuDeviceId));
+    gpuCheck(cudaMemPrefetchAsync(A, sizeof(double) * nzv, cudaCpuDeviceId));
+    gpuCheck(cudaMemPrefetchAsync(ARowPtr, sizeof(int) * (dimX + 1),
+                                  cudaCpuDeviceId));
+    gpuCheck(
+        cudaMemPrefetchAsync(AColIndx, sizeof(int) * nzv, cudaCpuDeviceId));
   }
 
-  // Initialize the sparse matrix
-  cputimer_start();
   matrixInit(A, ARowPtr, AColIndx, dimX, alpha);
-  cputimer_stop("Initializing the sparse matrix on the host");
 
-  // Initiliaze the boundary conditions for the heat equation
-  cputimer_start();
   memset(temp, 0, sizeof(double) * dimX);
   temp[0] = tempLeft;
   temp[dimX - 1] = tempRight;
-  cputimer_stop("Initializing memory on the host");
 
   if (concurrentAccessQ) {
-    cputimer_start();
-    //@@ Insert code to prefetch in Unified Memory asynchronously to the GPU
-    cudaMemPrefetchAsync(temp, dimX * sizeof(double), device, 0);
-    cudaMemPrefetchAsync(tmp, dimX * sizeof(double), device, 0);
-    cudaMemPrefetchAsync(A, nzv * sizeof(double), device, 0);
-    cudaMemPrefetchAsync(ARowPtr, (dimX + 1) * sizeof(int), device, 0);
-    cudaMemPrefetchAsync(AColIndx, nzv * sizeof(int), device, 0);
-    cputimer_stop("Prefetching GPU memory to the device");
+    gpuCheck(cudaMemPrefetchAsync(temp, sizeof(double) * dimX, device));
+    gpuCheck(cudaMemPrefetchAsync(tmp, sizeof(double) * dimX, device));
+    gpuCheck(cudaMemPrefetchAsync(A, sizeof(double) * nzv, device));
+    gpuCheck(cudaMemPrefetchAsync(ARowPtr, sizeof(int) * (dimX + 1), device));
+    gpuCheck(cudaMemPrefetchAsync(AColIndx, sizeof(int) * nzv, device));
   }
 
-  //@@ Insert code to create the cuBLAS handle
   cublasCheck(cublasCreate(&cublasHandle));
-
-  //@@ Insert code to create the cuSPARSE handle
   cusparseCheck(cusparseCreate(&cusparseHandle));
-
-  //@@ Insert code to set the cuBLAS pointer mode to CUBLAS_POINTER_MODE_HOST
   cublasCheck(cublasSetPointerMode(cublasHandle, CUBLAS_POINTER_MODE_HOST));
 
-  //@@ Insert code to call cusparse api to create the sparse matrix descriptor
-  //(generic API)
   cusparseCheck(cusparseCreateCsr(&matA, dimX, dimX, nzv, ARowPtr, AColIndx, A,
                                   CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I,
                                   CUSPARSE_INDEX_BASE_ZERO, CUDA_R_64F));
 
-  //@@ Insert code to create dense vector descriptors
-  cusparseCheck(cusparseCreateDnVec(&vecTemp, dimX, temp, CUDA_R_64F));
-  cusparseCheck(cusparseCreateDnVec(&vecTmp, dimX, tmp, CUDA_R_64F));
+  cusparseCheck(cusparseCreateDnVec(&vecX, dimX, temp, CUDA_R_64F));
+  cusparseCheck(cusparseCreateDnVec(&vecY, dimX, tmp, CUDA_R_64F));
 
-  //@@ Insert code to call cusparse api to get the buffer size needed by SpMV
   cusparseCheck(cusparseSpMV_bufferSize(
-      cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &one, matA, vecTemp,
-      &zero, vecTmp, CUDA_R_64F, CUSPARSE_SPMV_ALG_DEFAULT, &bufferSize));
+      cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &one, matA, vecX, &zero,
+      vecY, CUDA_R_64F, CUSPARSE_SPMV_ALG_DEFAULT, &bufferSize));
 
-  //@@ Insert code to allocate the buffer needed by cuSPARSE
-  cudaMalloc(&buffer, bufferSize);
+  gpuCheck(cudaMalloc(&buffer, bufferSize));
 
-  // Perform the time step iterations
+  gpuCheck(cudaEventCreate(&spmv_start));
+  gpuCheck(cudaEventCreate(&spmv_stop));
+
   for (int it = 0; it < nsteps; ++it) {
-    //@@ Insert code to call cusparse api to compute the SpMV (sparse
-    //matrix-vector multiplication)
-    //@@ This calculation corresponds to: tmp = 1 * A * temp + 0 * tmp
+
+    gpuCheck(cudaEventRecord(spmv_start));
+
     cusparseCheck(cusparseSpMV(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE,
-                               &one, matA, vecTemp, &zero, vecTmp, CUDA_R_64F,
+                               &one, matA, vecX, &zero, vecY, CUDA_R_64F,
                                CUSPARSE_SPMV_ALG_DEFAULT, buffer));
 
-    //@@ Insert code to call cublas api to compute the axpy routine using
-    //cuBLAS.
-    //@@ This calculation corresponds to: temp = alpha * tmp + temp
-    cublasCheck(cublasDaxpy(cublasHandle, dimX, &alpha, tmp, 1, temp, 1));
+    gpuCheck(cudaEventRecord(spmv_stop));
+    gpuCheck(cudaEventSynchronize(spmv_stop));
+    gpuCheck(cudaEventElapsedTime(&spmv_time_ms, spmv_start, spmv_stop));
+    spmv_total_time_ms += spmv_time_ms;
 
-    //@@ Insert code to call cublas api to compute the norm of the vector using
-    //cuBLAS
-    //@@ This calculation corresponds to: ||temp||
+    cublasCheck(cublasDaxpy(cublasHandle, dimX, &alpha, tmp, 1, temp, 1));
     cublasCheck(cublasDnrm2(cublasHandle, dimX, temp, 1, &norm));
 
-    // If the norm of A*temp is smaller than 10^-4 exit the loop
     if (norm < 1e-4)
       break;
   }
 
-  // Calculate the exact solution using thrust
+  printf("Total SpMV time: %.6f seconds\n", spmv_total_time_ms / 1000.0);
+
   thrust::device_ptr<double> thrustPtr(tmp);
   thrust::sequence(thrustPtr, thrustPtr + dimX, tempLeft,
                    (tempRight - tempLeft) / (dimX - 1));
 
-  // Calculate the relative approximation error:
   one = -1;
-  //@@ Insert the code to call cublas api to compute the difference between the
-  //exact solution
-  //@@ and the approximation
-  //@@ This calculation corresponds to: tmp = -temp + tmp
   cublasCheck(cublasDaxpy(cublasHandle, dimX, &one, temp, 1, tmp, 1));
-
-  //@@ Insert the code to call cublas api to compute the norm of the absolute
-  //error
-  //@@ This calculation corresponds to: || tmp ||
   cublasCheck(cublasDnrm2(cublasHandle, dimX, tmp, 1, &norm));
 
   error = norm;
-  //@@ Insert the code to call cublas api to compute the norm of temp
-  //@@ This calculation corresponds to: || temp ||
   cublasCheck(cublasDnrm2(cublasHandle, dimX, temp, 1, &norm));
+  error /= norm;
 
-  // Calculate the relative error
-  error = error / norm;
   printf("The relative error of the approximation is %f\n", error);
 
-  //@@ Insert the code to destroy the descriptors
-  cusparseCheck(cusparseDestroyDnVec(vecTemp));
-  cusparseCheck(cusparseDestroyDnVec(vecTmp));
-  cusparseCheck(cusparseDestroySpMat(matA));
+  gpuCheck(cudaEventDestroy(spmv_start));
+  gpuCheck(cudaEventDestroy(spmv_stop));
 
-  //@@ Insert the code to destroy the cuSPARSE handle
-  cusparseCheck(cusparseDestroy(cusparseHandle));
+  cusparseDestroySpMat(matA);
+  cusparseDestroyDnVec(vecX);
+  cusparseDestroyDnVec(vecY);
+  cusparseDestroy(cusparseHandle);
+  cublasDestroy(cublasHandle);
 
-  //@@ Insert the code to destroy the cuBLAS handle
-  cublasCheck(cublasDestroy(cublasHandle));
-
-  //@@ Insert the code for deallocating memory
-  cudaFree(temp);
-  cudaFree(tmp);
-  cudaFree(A);
-  cudaFree(ARowPtr);
-  cudaFree(AColIndx);
-  cudaFree(buffer);
+  gpuCheck(cudaFree(temp));
+  gpuCheck(cudaFree(tmp));
+  gpuCheck(cudaFree(A));
+  gpuCheck(cudaFree(ARowPtr));
+  gpuCheck(cudaFree(AColIndx));
+  gpuCheck(cudaFree(buffer));
 
   return 0;
 }
--- a4/REPORT.md
+++ a4/REPORT.md
@@ -11,12 +11,49 @@ Both had equal contributions.
 
 ### 1)
 
-Run the program with different dimX values. For each one, explain how you can approximate the FLOPS (floating-point operation per second) achieved in computing the SMPV (sparse matrix multiplication). Report FLOPS achieved by your code at different input sizes.
+To approximate the FLOPS achieved in computing the SMPV, we can find the theoretical required number of floating-point operations required to compute the SMPV and then find the approximate time spent executing the SMPV. If we divide the theoretical number of FLOP's by the approximate time we can find the approximate FLOPS achieved during execution. This will not be exact due to the inaccuracy of time measurement and the likely more required FLOP's required by the actual cuSPARSE implementation.
+
+By the definition of the diffusion matrix $A$ and the time step vector `tmp`, we can see that to compute the SMPV for a given time step we will need to perform 4 operations per element, for $dimX-2$ elements (as the first and last row in `tmp` are the constant 0). Thus for the entire computation we will need to perform $nsteps \cdot 4(dimX-2)$ FLOP's.
+To approximate the time taken to compute the SMPV, we can utilise CUDA events to take time measurements before and after evaluation. Aggregating the individual time intervals over the time steps then results in a total execution time for computing the SMPV over the entire computation.
+
+| dimX | SPMV time (s) | FLOPS (op/s) |
+|------|---------------|--------------|
+| 1024 | 0.0261 | 156,676,375.90 |
+| 2048 | 0.0247 | 331,617,974.80 |
+| 4096 | 0.0253 | 64,668,4831.97 |
+| 8192 | 0.0235 | 1,393,746,011.49 |
+| 16384 | 0.0299 | 2,192,158,437.04 |
+| 32768 | 0.0252 | 5,210,670,695.34 |
+| 65536 | 0.0334 | 7,849,793,376.06 |
+| 131072 | 0.0486 | 10,791,429,100.71 |
+| 262144 | 0.0957 | 10,953,504,162.79 |
+| 524288 | 0.1734 | 12,095,720,935.06 |
+
+As dimX increases, the measured SPMV time increases as expected due to the larger workload. In addition, the approximate FLOPS increases: this is likely due to the decreased ratio of overhead of calling the various kernels involved in the cuSPARSE implementation.
 
 ### 2)
 
-Run the program with dimX=1024 and vary nsteps from 100 to 10000. Plot the relative error of the approximation at different nstep. Describe and explain your observations?
+Running the program 100 times with dimX=1024 for nsteps from 100 to 10000, we can find how the relative error of the approximation changes as the number of time steps increases in the following plot:
+
+![Q2.2: Relative error for various timesteps](Q2/relative_error.png "immagine")
+
+We can see from the plot that the relative error seems to converge as the number of time steps increases. Initially there are large decreases in relative error, but these decreases are smaller for the later time steps. This aligns with our intuition of a diffusion system as the system converges to a stable solution.
 
 ### 3)
 
-Compare the performance with and without the prefetching in Unified Memory. How is the performance impact? [Optional: using nvprof or ncu to get UM related metrics]
+The below table shows the average execution time over 20 iterations for various dimX sizes, with and without prefetching.
+
+| dimX | Without prefetching (s) | With prefetching (s) |
+|------|-------------------------|----------------------|
+| 64 | 0.452827 | 0.441194 |
+| 128 | 0.470727 | 0.383741 |
+| 256 | 0.472843 | 0.403734 |
+| 512 | 0.481950 | 0.452170 |
+| 1024 | 0.531924 | 0.427101 |
+| 2048 | 0.493554 | 0.427965 |
+| 4096 | 0.516902 | 0.454684 |
+| 8192 | 0.506130 | 0.422726 |
+| 16384 | 0.490952 | 0.447741 |
+| 32768 | 0.537801 | 0.438596 |
+
+There seems to be a slight decrease in execution time with prefetching, with this difference being more pronounced for larger input values. This aligns with our intuition that as the required data for computation increases in size, the penalty of having to move data across devices increases as well.
